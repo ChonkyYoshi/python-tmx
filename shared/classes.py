4a0ad7bbc47836cc76168be2858b72c538f71be8
Enzo Agosta <agosta.enzowork@gmail.com>
2023-10-24 12:50:57 +0200

    a

--- shared/classes.py
+++ shared/classes.py
@@ -1,120 +1,60 @@
-from dataclasses import dataclass
-from datetime import datetime
-from pathlib import Path
-import enums
-import lxml.etree as et
+"""shared classes definitions"""
+from lxml.etree import _Element, Element
+
+from abc import ABC
+from typing import Any
+from re import search
+
+xml = "{http://www.w3.org/XML/1998/namespace}"
 
 
-@dataclass
-class prop:
-    text: str
-    prop_type: str
-    lang: str
-    encoding: str
+class StructuralElement(ABC):
+    def __init__(self) -> None:
+        self.name = self.__class__.__name__
+        super().__init__()
+
+    @property
+    def _tmx_attrib(self) -> dict:
+        attrs: dict = dict()
+        for key in vars(self).keys():
+            if key == "lang":
+                attrs[f"{xml}lang"] = getattr(self, key)
+            elif key in ["tmf", "encoding"]:
+                attrs[f"o-{key}"] = getattr(self, key)
+            elif search(r"\w+_type$", key):
+                attrs["type"] = getattr(self, key)
+            else:
+                attrs[key] = getattr(self, key)
+        return attrs
+
+    @property
+    def _element(self) -> _Element:
+        elem: _Element = Element(self.name)
+        for key, value in self._tmx_attrib.items():
+            elem.set(key, str(value))
+        return elem
 
 
-@dataclass
-class note:
-    text: str
-    lang: str
-    encoding: str
+class prop(StructuralElement):
+    """StructuralElement subclass reprenting a <prop> tag.\n
 
+    Requried attributes:\n
+        - prop_type\n
+        - value\n
+    Optional attributes (None by default):\n
+        - lang\n
+        - encoding\n
+    """
 
-@dataclass
-class run:
-    text: str
-
-
-@dataclass
-class bpt(run):
-    i: str | int
-    x: str | int
-    type: str
-
-
-@dataclass
-class ept(run):
-    i: str | int
-
-
-@dataclass
-class hi(run):
-    pass
-
-
-@dataclass
-class it(run):
-    pos: enums.Pos
-    x: int | str
-    type: str
-
-
-@dataclass
-class ph(run):
-    x: int | str
-    type: str
-    assoc: enums.Assoc
-
-
-@dataclass
-class tuv:
-    runs: list[run]
-    lang: str
-    encoding: str
-    datatype: str
-    usagecount: int | str
-    lastusagedate: str | datetime
-    creationtool: str
-    creationtoolversion: str
-    creationdate: str | datetime
-    creationid: str
-    changedate: str | datetime
-    changeid: str
-    tmf: str
-    props: list[prop]
-    notes: list[note]
-
-
-@dataclass
-class tu:
-    tuvs: list[tuv]
-    tuid: str
-    encoding: str
-    datatype: str
-    usagecount: int | str
-    lastusagedate: str | datetime
-    creationtool: str
-    creationtoolversion: str
-    creationdate: str | datetime
-    creationid: str
-    changedate: str | datetime
-    changeid: str
-    segtype: enums.Segtype
-    tmf: str
-    srclang: str
-    props: list[prop]
-    notes: list[note]
-
-
-@dataclass
-class header:
-    creationtool: str
-    creationtoolversion: str
-    segtype: enums.Segtype
-    tmf: str
-    adminlang: str
-    srclang: str
-    datatype: str
-    encoding: str
-    creationdate: str | datetime
-    creationid: str
-    changedate: str | datetime
-    changeid: str
-    props: list[prop]
-    notes: list[note]
-
-
-@dataclass
-class tmx:
-    header: header
-    tus: list[tu]
+    def __init__(
+        self,
+        prop_type: str,
+        value: Any,
+        lang: str | None = None,
+        encoding: str | None = None,
+    ) -> None:
+        self.prop_type: str = prop_type
+        self.value: Any = value
+        self.lang: str | None = lang
+        self.encoding: str | None = encoding
+        super().__init__()
