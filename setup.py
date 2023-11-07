9d38124b2a99af6b26a0b1baabe92fa2bfae1da6
Enzo Agosta <agosta.enzowork@gmail.com>
2023-11-07 14:07:41 +0100

    added setup files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-tmx",
+    version="0.1",
+    author="Enzo Agosta",
+    author_email="agosta.enzowork@gmail.com",
+    description="Python library for manipulating tmx files",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
