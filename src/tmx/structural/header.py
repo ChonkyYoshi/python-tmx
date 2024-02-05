963ff97d12d85670fd6303a85b1534db9bb90750
Enzo Agosta <agosta.enzowork@gmail.com>
2024-02-05 18:29:57 +0100

    Header Validation + Serialization

--- src/tmx/structural/header.py
+++ src/tmx/structural/header.py
@@ -1,5 +1,5 @@
 from typing import Literal
-from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import Element, ElementTree
 from datetime import datetime, UTC
 from re import match
 
@@ -25,6 +25,7 @@ class header:
 
     def __init__(
         self,
+        _validate: bool,
         creationtool: str,
         creationtoolversion: str,
         segtype: Literal["block", "paragraph", "sentence", "phrase"],
@@ -46,52 +47,65 @@ class header:
         self.adminlang = adminlang
         self.srclang = srclang
         self.datatype = datatype
-        self._ValidateAttributes()
+        if _validate:
+            self._ValidateAttributes()
 
     def _ValidateAttributes(self) -> None:
         for attr in self.__slots__:
             attr_value = getattr(self, attr)
-            if attr_value is None:
-                continue
-            if attr.endswith("date"):
-                if type(attr_value) == datetime:
-                    if (
-                        attr_value.tzinfo is None
-                        or attr_value.tzinfo.utcoffset(attr_value) is None
-                    ):
-                        attr_value = attr_value.astimezone(tz=UTC)
-                    else:
-                        attr_value = datetime.astimezone(attr_value, UTC)
-                    setattr(self, attr, attr_value.strftime("%Y%m%dT%H%M%SZ"))
+            match attr_value:
+                case None:
                     continue
-                else:
-                    try:
-                        datetime.strptime(str(attr_value), "%Y%m%dT%H%M%SZ")
-                    except ValueError:
-                        raise ValueError(
-                            f"""{attr_value} is not a correct value for {attr}.
-{attr} should be an aware datetime object (with its timezone set to UTC) or a str in the format YYYYMMDDTHHMMSSZ"""
+                case datetime():
+                    if not attr.endswith("date"):
+                        raise TypeError(
+                            f"datetime objects cannot be used for attribute {attr}"
                         )
-            if attr.endswith("lang"):
-                if not match(r"^[a-z][a-z](-[a-z][a-z][a-z]?)?$", attr_value.lower()):
-                    raise ValueError(
-                        f"""{attr_value} is not a correct value for {attr}
+                    continue
+                case str():
+                    if attr.endswith("lang"):
+                        if not match(
+                            r"^[a-z][a-z](-[a-z][a-z][a-z]?)?$", attr_value.lower()
+                        ):
+                            raise ValueError(
+                                f"""{attr_value} is not a correct value for {attr}
 {attr} should be in the format xx(-YY)
 where xx is a 2 letter ISO country code and YY is an optional 2 or 3 letter ISO language code"""
+                            )
+                        continue
+                    if attr == "segtype" and attr_value not in [
+                        "block",
+                        "paragraph",
+                        "sentence",
+                        "phrase",
+                    ]:
+                        raise ValueError(
+                            f"""{attr_value} is not a correct value for segtype.
+segtype should be one of block, paragraph, sentence or phrase."""
+                        )
+                    if attr.endswith("date"):
+                        datetime.strptime(attr_value, "%Y%m%dT%H%M%SZ")
+                        continue
+                case _:
+                    raise TypeError(
+                        f"only strings (and datetime objects for creationdate and changedate) can be used as values for header attribute not {type(attr_value)}"
                     )
-                setattr(self, attr, attr_value.lower())
-                continue
 
     def _XmlElement(self) -> Element:
         self._ValidateAttributes()
+        attribs: dict = {
+            attr: getattr(self, attr)
+            for attr in self.__slots__
+            if getattr(self, attr) is not None
+        }
         return Element(
             "header",
             {
-                attr: getattr(self, attr)
-                for attr in self.__slots__
-                if getattr(self, attr) is not None
+                key: (
+                    value
+                    if not isinstance(value, datetime)
+                    else datetime.strftime(value, "%Y%m%dT%H%M%SZ")
+                )
+                for key, value in attribs.items()
             },
         )
-
-
-a = header("1", "1", "block", "1", "en", "en", "1", creationdate=13)
