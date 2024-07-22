6e1303f3d190a4a4d5df9addda827a0f1b2f0166
Enzo <agosta.enzowork@gmail.com>
2024-07-22 15:53:40 +0200

    start from scratch, want to find a pattern that works

--- PythonTmx/__init__.py
+++
@@ -1,8 +0,0 @@
-from logging import DEBUG, FileHandler, Formatter, getLogger
-
-logger = getLogger("PythonTmx Logger")
-logger.setLevel(DEBUG)
-format_ = Formatter("%(asctime)s: %(levelname)s - %(funcName)s - \n%(message)s")
-file_handler = FileHandler("logs.log", mode="w", encoding="utf-8")
-file_handler.setFormatter(format_)
-logger.addHandler(file_handler)
