b347a80a7ead90ec5567870dccbcfad99d368f49
EnzoAgosta <enzo.agosta@ninjarmm.com>
2024-07-25 18:19:02 +0200

    need to make iterparse

--- PythonTmx/__init__.py
+++ PythonTmx/__init__.py
@@ -1,16 +1,18 @@
 from io import BytesIO, StringIO
 from os import PathLike
 
-from lxml.etree import XMLParser, _Element, _ElementTree, parse
+from lxml.etree import XMLParser, XMLPullParser, _Element, _ElementTree, parse, iterparse
 
 from PythonTmx.core import Tmx
 
 
-def read_from_file(file: str | bytes | PathLike | StringIO | BytesIO) -> Tmx:
-    tree: _ElementTree = parse(
-        file, XMLParser(remove_blank_text=True, resolve_entities=False)
-    )
-    root: _Element = tree.getroot()
-    if root.tag != "tmx":
-        raise ValueError(f"Expected root tag to be <tmx> but got {root.tag}")
-    return Tmx(source_element=root)
+def read_from_file(file: str | bytes | PathLike | StringIO | BytesIO, use_pull: bool) -> Tmx:
+    if not use_pull:
+        tree: _ElementTree = parse(file, XMLParser(remove_blank_text=True))
+        root: _Element = tree.getroot()
+        if root.tag != "tmx":
+            raise ValueError(f"Expected root tag to be <tmx> but got {root.tag}")
+        return Tmx(source_element=root)
+    else:
+        for event, elem in iterparse(file):
+            
